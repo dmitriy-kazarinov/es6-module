757eb9e8c7446f6d4a351ae7d4ba26e98e746154
dmitrydulin <dmitry.dulin@ainstainer.de>
2016-01-26 14:26:56 +0200

    master del

--- app/main.js
+++
@@ -1,8 +0,0 @@
-import {Company,start} from './module1.js';
-import {Ainstainer} from './module2.js';
-
-let ain = new Ainstainer("Ainstainer","Ukraine");
-console.log(ain.aboutString());
-
-var div = document.getElementByid('about');
-div.innerHTML = div.innerHTML + ain.aboutString();
--- app/module2.js
+++
@@ -1,6 +0,0 @@
-import {Company} from './module1.js';
-export class Ainstainer extends Company {
-    aboutString() {
-        return super.aboutString();
-    }
-}
